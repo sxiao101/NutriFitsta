0f071b5001e8c315ed239550e34660c8b5270b81
sxiao101 <sxiao1@fb.com>
2021-07-13 14:46:15 -0400

    Bottom tab bar navigation

--- app/src/main/java/com/codepath/nutrifitsta/MainActivity.java
+++ app/src/main/java/com/codepath/nutrifitsta/MainActivity.java
@@ -1,14 +1,51 @@
 package com.codepath.nutrifitsta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.codepath.nutrifitsta.fragments.PostsFragment;
+import com.codepath.nutrifitsta.fragments.ProfileFragment;
+import com.codepath.nutrifitsta.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.action_home: fragment = new PostsFragment();
+                        break;
+                    case R.id.action_search: fragment = new SearchFragment();
+                        break;
+                    case R.id.action_profile:fragment = new ProfileFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+    }
+
+    public void switchContent(int id, Fragment fragment) {
+        fragmentManager.beginTransaction().replace(id, fragment).addToBackStack(null).commit();
     }
 }
